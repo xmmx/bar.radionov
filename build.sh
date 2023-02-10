972b579d479515fe11377f53c0a677da6d0e189c
Vitaly Radionov <vitaly.radionov@anychart.com>
2023-02-11 00:32:25 +0800

    v1.0.1

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 echo "Remove old build..."
-rm -rf public
+rm -rf docs
 
 echo "Copy files..."
-cp -r src public
-cd public
+cp -r src docs
+cd docs
 unlink test.html
 unlink index-source.html
 unlink js/main.js
@@ -12,10 +12,10 @@ rm -rf styles
 cd ../
 
 echo "Minify css..."
-npx css-minify -d src/styles/ -o public/styles
+npx css-minify -d src/styles/ -o docs/styles
 
 echo "Minify js..."
-npx minify src/js/main.js > public/js/main.min.js
+npx minify src/js/main.js > docs/js/main.min.js
 
 echo "Collect assets for PWA..."
 ./collect_assets.js
--- collect_assets.js
+++ collect_assets.js
@@ -4,19 +4,19 @@ import fs from 'fs';
 import path from 'path';
 
 const assets = [
-  "/",
-  "/index.html"
+  "./",
+  "./index.html"
 ]
 
 const folders = ['styles', 'js', 'img']
 folders.forEach(folder => {
-  fs.readdirSync(path.join('./', 'static', folder)).forEach(file => {
+  fs.readdirSync(path.join('./', 'docs', folder)).forEach(file => {
     if (file != '.DS_Store')
-      assets.push(`/${folder}/${file}`);
+      assets.push(`./${folder}/${file}`);
   });
 })
 
-const filename = path.join('./', 'static', 'worker.js');
+const filename = path.join('./', 'docs', 'worker.js');
 const workerContent = fs.readFileSync(filename);
 const newContent = `const assets = ${JSON.stringify(assets)}\n${workerContent}`;
 fs.writeFileSync(filename, newContent);
